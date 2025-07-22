d790666b965ec88c80303cb3db63831cb9f5f8fa
Caique Gargel <caiquegargeloliveira@gmail.com>
2025-07-21 23:09:12 -0300

    fix cors 3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ readOnly: true });
+
 
 // Lista de origens permitidas
 const allowedOrigins = [
@@ -26,7 +26,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use(middlewares);
+
 
 // Reescreve rotas
 server.use(
@@ -38,17 +38,6 @@ server.use(
 // Rotas do JSON Server
 server.use(router);
 
-// Força CORS também **depois do router**
-server.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 server.listen(3000, () => {
   console.log("JSON Server is running");
